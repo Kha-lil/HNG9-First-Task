f2ddeab66df778037a1ba48b42a7629bb47d3b50
Ikumapayi Khalilullah Olaniyi <ikumapayiolaniyi007@gmail.com>
2022-11-05 17:25:48 +0100

    task 2 done

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const cors = require("cors");
 const app = express();
 const port = 8000;
 
-app.use(cors())
+app.use(cors());
+app.use(express.json())
 
 const responseData = {
   slackUsername: "khaleel",
@@ -14,7 +15,35 @@ const responseData = {
 };
 
 app.get("/", (req, res) => {
-  res.json({ ...responseData });
+  res.status(200).json({ ...responseData });
+});
+
+app.post("/", (req, res) => {
+  const { operation_type, x, y } = req.body;
+  console.log(operation_type, x, y);
+  const { slackUsername } = responseData;
+  if (!x) {
+    res.status(400).send("please provide a value for x");
+  }
+  if (!y) {
+    res.status(400).send("please provide a value for y");
+  }
+  if (!operation_type) {
+    res.status(400).send("please provide an operation type");
+  }
+
+  let result;
+
+  if (operation_type === "addition") {
+    result = x + y;
+  }
+  if (operation_type === "subtraction") {
+    result = x - y;
+  }
+  if (operation_type === "multiplication") {
+    result = x * y;
+  }
+  res.status(200).json({ slackUsername, result, operation_type });
 });
 
 app.listen(port, () => {
